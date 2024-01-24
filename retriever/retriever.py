4b8aa2a4df0250a0c4f159e1a34e39947fc2dc80
aibakeneko <aibakeneko@gmail.com>
2024-01-24 17:10:36 +0100

    Updated code

--- retriever/retriever.py
+++ retriever/retriever.py
@@ -3,6 +3,7 @@ import os
 import re
 import sys
 import typer
+import roman
 
 # import openpyxl
 import pandas as pd
@@ -34,6 +35,9 @@ def get_toc(filename, toc_name, max_not_matched_lines):
             if match:
                 title, source, date_str = match.groups()
                 date = date_str
+                # FIXME Warn duplicate entries
+                # if any(title in row for row in data):
+                #     logger.warning(f"Duplicate entry found in {filename} for '{title}', {source}, {date}")
                 data.append([title, source, date, toc_line_number])
                 # Reset not matched lines counter
                 not_matched_lines = 0
@@ -57,7 +61,7 @@ def get_articles(filename: str, offset) -> list[str]:
     return articles
 
 
-def create_db(toc, articles):
+def create_corpus(toc, articles):
     for i, article in enumerate(articles):
         toc_entry = toc[i]
         title, source, date, _ = toc_entry
@@ -159,7 +163,9 @@ def main(input_folder: str) -> None:
         filepath: str = f"{input_folder}/{filename}"
         toc, offset = get_toc(filepath, "Innehållsförteckning:", 2)
         articles: list[str] = get_articles(filepath, offset)
-        df: pd.DataFrame = create_db(toc, articles)
+
+
+        df: pd.DataFrame = create_corpus(toc, articles)
 
         df['document_name'] = df.source.fillna('').str.replace(r'\W+', '_', regex=True).str.lower().str.strip()
         df['document_name'] = df.document_name + '_' + df.title.fillna('').str.replace(r'\W+', '_', regex=True).str.lower().str.strip().str.strip('_').str[:60]
@@ -167,6 +173,10 @@ def main(input_folder: str) -> None:
         df['filename'] = df.document_name + ".txt"
         df['year'] = df.date.fillna(0).str[:4].astype(int)
         df["input_file"] = filename
+
+
+        df["id"] = df.input_file.str.rsplit('_').str[-1].str.replace('.txt', '').apply(lambda x: roman.fromRoman(x)).astype(int).astype(str).str.zfill(3) + df.index.astype(str).str.zfill(3)
+
         article_counts[filename] = len(df)
         metadata = df.drop(columns=["article_text", "full_text", "header", "toc_line_number"])
 
@@ -179,16 +189,41 @@ def main(input_folder: str) -> None:
         # Save articles to txt files
         save_articles(output_folder, filename, df)
 
-    # Save all_metadata to excel
+    # Save all_metadata
     document_index: pd.DataFrame = pd.concat(all_metadata, ignore_index=True)
     document_index.reset_index(drop=True, inplace=True)
     document_index['document_id'] = document_index.index
     # document_index.to_excel(f"{output_folder}/metadata.xlsx", index=False)
-    # save to csv
 
+    # Find duplicates
+    # duplicates = document_index[document_index.duplicated(subset=['title', 'source', 'date', 'media'], keep=False)]
+    # duplicates = duplicates.groupby(['title', 'source', 'date', 'media']).agg({'url':[lambda x: ', '.join(x), 'count']}).reset_index()
+
+    duplicates = document_index[document_index.duplicated(subset=['document_name', 'source', 'date', 'media'], keep=False)]
+    duplicates = duplicates.groupby(['document_name', 'source', 'date', 'media']).agg({'url':[lambda x: ', '.join(x), 'count']}).reset_index()
+
+
+    duplicates.columns = ['_'.join(col).strip() if col[1] else col[0] for col in duplicates.columns.values]
+    duplicates = duplicates.rename(columns={'url_<lambda_0>': 'urls', 'url_count': 'count'})
+    duplicates.to_csv(f"{output_folder}/duplicates.csv", index=True, sep=";", encoding="utf-8-sig")
+
+    # Remove duplicates
+    document_index.drop_duplicates(subset=['title', 'source', 'date', 'media'], inplace=True)
+
+    # Save document_index to csv
     document_index.to_csv(f"{output_folder}/document_index.csv", index=False, sep=";", encoding="utf-8-sig")
 
     print(article_counts)
+    print(sum(article_counts.values()))
+    print(len(document_index))
+
+    print(f'Number of articles (article_counts): {sum(article_counts.values())}')
+    print(f'Number of unique articles (document index): {len(document_index)}')
+    print(f'Number of duplicates: {len(duplicates)}')
+
+    # print number of txt files in output folder
+    print(f'Number of txt files in output folder: {len(os.listdir(output_folder))}')
+
 if __name__ == "__main__":
     #typer.run(main)
     logger.remove()
